5baafb8dcd6e146c71ec3793adcb374c352b05ae
Your Name <you@example.com>
2026-04-25 21:15:57 -0500

    fix deploy to use schtasks

--- deploy.bat
+++ deploy.bat
@@ -3,5 +3,5 @@ echo Deploying Children Of Promise...
 cd /d C:\app
 "C:\Program Files\Git\bin\git.exe" pull
 taskkill /F /IM node.exe 2>nul
-net start CofPServer
+schtasks /run /tn "CofPServer"
 echo Deploy complete!
